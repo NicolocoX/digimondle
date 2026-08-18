4055514f06e6f952df9f3dd0a002dd5db8b13b6c
NicolocoX <namsnicolas@gmail.com>
2026-08-17 20:19:10 -0400

    estilo buscador

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,4 +1,5 @@
 import Cascada from "./Cascada"
+import "./Buscador.css"
 
 export default function Buscador({ resultados }) {
   const handleOnSubmit = (event) => {
@@ -8,9 +9,12 @@ export default function Buscador({ resultados }) {
   }
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <label>Buscar: </label>
-      <input />
+    <form className="buscador" onSubmit={handleOnSubmit}>
+      <div className="entrada">
+        <label>Buscar: </label>
+        <input />
+      </div>
+
       <Cascada resultados={resultados} />
     </form>
   )
