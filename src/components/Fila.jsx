04e8f03c77304572c378842a1ff932b29b116d51
NicolocoX <namsnicolas@gmail.com>
2026-08-18 23:42:27 -0400

    aparece imagen en las filas

--- src/components/Fila.jsx
+++ src/components/Fila.jsx
@@ -1,7 +1,8 @@
 import Casilla from "./Casilla"
+import "./Fila.css"
 
 export default function Fila({ digimon }) {
-  const nombre = digimon.name
+  const imagen = digimon.images[0].href
   const nivel = digimon.levels.length ? digimon.levels[0].level : "Sin información"
   const atributo = digimon.attributes.length ? digimon.attributes[0].attribute : "Sin información"
   const campo = digimon.fields.length ? digimon.fields[0].field : "Sin información"
@@ -9,8 +10,8 @@ export default function Fila({ digimon }) {
 
 
   return (
-    <div>
-      <Casilla>{nombre}</Casilla>
+    <div className="fila">
+      <Casilla><img src={imagen} alt={digimon.name} /></Casilla>
       <Casilla>{nivel}</Casilla>
       <Casilla>{atributo}</Casilla>
       <Casilla>{campo}</Casilla>
