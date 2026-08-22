765ec2c22df3131232eef6bfe0b3d314886ebea7
NicolocoX <namsnicolas@gmail.com>
2026-08-21 23:23:54 -0400

    casillas scrolleables

--- src/components/Casilla.jsx
+++ src/components/Casilla.jsx
@@ -2,6 +2,8 @@ import "./Casilla.css"
 
 export default function Casilla({ children, tipo = "" }) {
   return (
-    <div className={`casilla${tipo}`}>{children}</div>
+    <div className={`casilla${tipo}`}>
+      {Array.isArray(children) ? "\n" + children.join("\n") : children}
+    </div>
   )
 }
