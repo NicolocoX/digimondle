19e79576b6bd3e16f4fae63461eca8549eefd28d
NicolocoX <namsnicolas@gmail.com>
2026-08-26 19:02:32 -0400

    estilo modal perdedor

--- src/components/AnuncioGanador.jsx
+++ src/components/AnuncioGanador.jsx
@@ -1,9 +1,11 @@
 import "./AnuncioGanador.css"
 
 export default function AnuncioGanador({ nombre, imagen, setMostrarModal, partidaGanada }) {
+  const ganador = partidaGanada ? "" : " perdedor"
+
   return (
     <div className="anuncio-ganador" onClick={() => setMostrarModal(false)}>
-      <div className="anuncio-ganador-modal">
+      <div className={`anuncio-ganador-modal${ganador}`}>
         <h1>{partidaGanada ? "¡HAS GANADO!" : "Perdiste"}</h1>
         <img src={imagen} alt={nombre} />
         <span>{partidaGanada ? "Encontraste a " : "Tu objetivo era "}{nombre}</span>
